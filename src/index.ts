cbe870f6aaf0ea10399b64a2994d0e8c96fa3cc9
lintangwisesa <lintangwisesa@ymail.com>
2020-10-15 16:14:32 +0700

    init theme

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * A plugin for lin26/jupyterlab_tokopedia
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '@lin26/jupyterlab_tokopedia:plugin',
+  requires: [IThemeManager],
+  activate: function(app: JupyterFrontEnd, manager: IThemeManager) {
+    const style = '@lin26/jupyterlab_tokopedia/index.css';
+
+    manager.register({
+      name: 'jupyterlab_tokopedia',
+      isLight: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  },
+  autoStart: true
+};
+
+export default plugin;
